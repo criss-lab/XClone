47043e3d3612dbd68ccee99055bbc7842b6d041b
criss-lab <africana763@gmail.com>
2026-05-04 11:46:34 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,15 @@ export class AppComponent {
   }
 
   async initializeApp() {
+    // 1. Tell Capgo the app is ready
     await CapacitorUpdater.notifyAppReady();
+
+    // 2. Listen for updates
+    CapacitorUpdater.addListener('updateAvailable', async (info) => {
+      console.log('Update found:', info);
+
+      await CapacitorUpdater.download();
+      await CapacitorUpdater.set();
+    });
   }
 }
